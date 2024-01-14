897ffb3e75e4729ca620309f507fda954abeac6a
aviaMader <100083767+aviaMader@users.noreply.github.com>
2024-01-14 15:26:07 +0200

    name

---
+++ Main.py
@@ -0,0 +1,7 @@
+"""a basic pyton file to test the functionality of the git repository"""
+
+def print_hi(name):
+    print(f'Hi, {name}')      # Press Ctrl+F8 to toggle the breakpoint.
+
+if __name__ == '__main__':
+    print_hi("Yehuda!!!!")
